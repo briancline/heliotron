99d0f754b39bdddf58e44e669d24157227a43107
Brian Cline <brian.cline@gmail.com>
2013-12-12 00:22:03 -0600

    Change module import to squash a code smell

--- heliotron/__init__.py
+++ heliotron/__init__.py
@@ -1,6 +1,6 @@
 #from requests import get
 from heliotron.bridge import Bridge
 from heliotron.light import Light
-import heliotron.presets
+from heliotron import presets
 
 __all__ = ['Bridge', 'Light', 'presets']
